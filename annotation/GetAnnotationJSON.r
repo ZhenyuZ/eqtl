33ab659bac109a8f2c5777fa90635e287e70bc9a
Zhenyu Zhang <shakuras@gmail.com>
2015-05-17 22:21:44 -0500

    Add default behavior to get all diesease types

--- annotation/GetAnnotationJSON.r
+++ annotation/GetAnnotationJSON.r
@@ -1,14 +1,20 @@
 # Get TCGA Annotation JSON list of the input "disease"
-GetAnnotationJSON <- function(disease) {
+# if no disease name designated, get annotation for all types
+GetAnnotationJSON <- function(disease="") {
   # load packages	
   require(RCurl)
   require(RJSONIO)
   
   # get annotation file and parce to json of dccAnnotation
-  url <- paste0("https://tcga-data.nci.nih.gov/annotations/resources/searchannotations/json?disease=", disease)
-  cat(paste("querying TCGA annotation database of disease", disease, "... ...\n"))
+  # if no disease designated, get all annotation information
+  if (disease == "") {
+    url <- "https://tcga-data.nci.nih.gov/annotations/resources/searchannotations/json?"
+    cat(paste("querying TCGA annotation database of all disease types ... ...\n"))
+  } else {  
+    url <- paste0("https://tcga-data.nci.nih.gov/annotations/resources/searchannotations/json?disease=", disease)
+    cat(paste("querying TCGA annotation database of disease", disease, "... ...\n"))
+  }  
   annot.file <- getURL(url)
   annot.json <- fromJSON(annot.file)$dccAnnotation
 }
-
 
