092f10e0c3a085dfda5b8b0e9cddd41c2a06b6e8
Zhenyu Zhang <shakuras@gmail.com>
2015-05-18 23:00:04 -0500

    make compatible with large annotation table

--- annotation/FilterByAnnotation.r
+++ annotation/FilterByAnnotation.r
@@ -1,27 +1,29 @@
-
 # Remove TCGA barcode that exists in the annotation database
 # Input: TCGA barcode
 #        Annotation tbl
 #        array of categoryId to keep.  To remove all annotation barcode, use "none". Default is c("6", "204"), corresponding to prior and synchronous malignancy
 #        recursive flag. Default is TRUE, to remove not only the exact barcode, but all derived barcode
 # Output: Filtered TCGA barcode
-FilterByAnnotation <- function(barcode, annot.tbl, keep=c("6", "204"), recursive=T) {  
+FilterByAnnotation <- function(barcodes, annot.tbl, keep=c("6", "204"), recursive=T) {
   # remove annotation with categoryId in character array "keep"
   barcodeToRemove <- unique(annot.tbl$item[! annot.tbl$categoryId %in% keep])
-  
+
   if (recursive) {
     # recursively find annotation barcode, as well as any derived barcode, from the query barcode
-    regexToRemove <- paste0("(", paste(barcodeToRemove, collapse="|"), ")")
-    indexToRemove <- which(grepl(regexToRemove, barcode))
-  } else { 
+    isFound <- sapply(barcodes, function(x) 
+                       (substr(x, 1, 12) %in% barcodeToRemove) | 
+                       (substr(x, 1, 15) %in% barcodeToRemove) | 
+                       (substr(x, 1, 16) %in% barcodeToRemove) | 
+                       (substr(x, 1, 19) %in% barcodeToRemove) | 
+                       (substr(x, 1, 20) %in% barcodeToRemove) | 
+                       (substr(x, 1, 23) %in% barcodeToRemove) | 
+                       (substr(x, 1, 24) %in% barcodeToRemove) | 
+                       (substr(x, 1, 27) %in% barcodeToRemove) | 
+                       (substr(x, 1, 28) %in% barcodeToRemove) )
+    barcodes <- barcodes[which(!isFound)]                  
+  } else {
     # only find annotation barcode of exact mach
-    indexToRemove <- which(barcode %in% barcodeToRemove)
-  } 
-   
-  # remove the barcode detected 
-  if(length(indexToRemove) > 0) {
-    barcode = barcode[-w]
+    barcodes <- barcodes[which(!barcodes %in% barcodeToRemove)]
   }
-  return(barcode)
+  return(barcodes)
 }
-
